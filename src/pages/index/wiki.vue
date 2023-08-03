e8e72b77b1990b76cc1d2f5133bae5752d1c3866
zhenhuaixiu <zhenhuaixiu@sznews.com>
2023-08-03 17:22:43 +0800

    chore

---
+++ src/pages/index/wiki.vue
@@ -0,0 +1,45 @@
+<template>
+  <view class="container" :style="{opacity:pageOpacity}">
+
+  </view>
+</template>
+
+<script>
+import { getShareObj } from "@/utils/share.js";
+import Config from "../../config/config";
+
+export default {
+  data() {
+    return {
+      pageOpacity: 0,
+    };
+  },
+  components: {
+  },
+  onLoad(e) {
+  },
+  onReady(e) {
+    this.pageOpacity = 1
+  },
+  onShareAppMessage(res) {
+    return getShareObj()
+  },
+  onShareTimeline(res) {
+    return getShareObj()
+  },
+  methods: {
+  }
+};
+</script>
+
+<style lang="scss">
+page {
+  background-color: #f8f8f8;
+}
+.container {
+  transition: all 0.5s linear;
+  font-size: 14px;
+  line-height: 24px;
+  position: relative;
+}
+</style>
